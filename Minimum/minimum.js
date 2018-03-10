f859bed4fb18c0409d53445dd42374181fd96ca6
DChandlerP <chandler.prince@gmail.com>
2018-03-10 10:18:43 -0600

    Did recursion exercise.

---
+++ Minimum/minimum.js
@@ -0,0 +1,18 @@
+const int1 = prompt("Enter the first number: ");
+const int2 = prompt("Enter the second Number: ");
+
+if (isNumeric(int1) && isNumeric(int2)) console.log(min(int1, int2));
+else console.log("We checked, one of the numbers you entered wasn't a number")
+
+function min(int1, int2) {return int1 >= int2 ? int1 : int2;}
+
+//Prompt takes strings. This functions looks at the ASCII value of each char in the string.
+function isNumeric(str) {
+    let code, i, len;
+
+    for (i = 0, len = str.length; i < len; i++) {
+        code = str.charCodeAt(i);
+        if (!(code > 47 && code < 58)) return false;
+    }
+    return true;
+}
---
+++ Recursion/recursion.js
@@ -0,0 +1,19 @@
+const int1 = prompt("Enter a number: ");
+if (isNumeric(int1)) console.log(isEven(int1));
+else console.log("I checked, what you entered wasn't a number");
+
+function isEven(n) {
+    if (n === 0 || n === 1) return n === 0;
+    else return n > 0 ? isEven(n-2) : isEven(n+2);
+}
+
+//Prompt takes strings. This functions looks at the ASCII value of each char in the string.
+function isNumeric(str) {
+    let code, i, len;
+
+    for (i = 0, len = str.length; i < len; i++) {
+        code = str.charCodeAt(i);
+        if (!(code > 47 && code < 58)) return false;
+    }
+    return true;
+}
