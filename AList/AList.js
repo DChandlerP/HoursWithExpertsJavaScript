3921b484ce60b18b89507d6195040a59795e87c0
DChandlerP <chandler.prince@gmail.com>
2018-03-21 15:07:37 -0500

    I had some trouble with AList and DeepComparison but found solutions.

--- AList/AList.js
+++ AList/AList.js
@@ -14,6 +14,31 @@ function arrayToList(arr) {
         list = { value: arr[i], rest: list };
     }
     return list;
-};
+}
+
+
+function listToArray(node) {
+    let arr = [];
+    //Uses JS magic where node is true until null because it's truthy
+    for(let list = node; node; node = node.rest) {
+        arr.push(node.value);
+    }
+    return arr;
+}
+
+function prepend(ele, list){
+    let arr = listToArray(list);
+    arr.unshift(ele);
+    return arrayToList(arr);
+}
+
+function nth(list, index) {
+    const arr = listToArray(list);
+    const nextIndex = (index + 1) > arr.length ? arr.length : (index +1);
+    return arr.slice(index, nextIndex).toString();
+}
 
 console.log(arrayToList([10, 20, 30]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
